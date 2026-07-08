bfb6b50c154f06eb44494d5da1f2f614a03e1846
Striking_AI <paraya.artificial@gmail.com>
2026-07-08 16:23:54 -0400

    CMS: Añadido nuevo perfil - Patricio Araya Briceño

---
+++ perfiles/striking-sites.com/data.js
@@ -0,0 +1,4 @@
+const portfolioData = {
+    personal: { name: "Patricio Araya Briceño", role: "ingeniero en Informatica", email: "no-reply@ejemplo.com", phone: "+56973933256", location: "Remoto", linkedin: "https://linkedin.com/patricio.araya", summary: "pruebas de datos" },
+    experience: [], education: [], skills: ["ingenieria","senior","informatica","ingles","avanzado"]
+};
